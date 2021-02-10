8e359268065ee6219c123ed3a9d0c085e8ccf334
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-10 18:59:41 +0000

    refactored input.js to accept WASD movement and write to connection. refactored play.js to pass conn to input.js. added functionality for canned messages.

--- input.js
+++ input.js
@@ -1,10 +1,13 @@
 //input.js
 
+let connection;
+
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -17,9 +20,43 @@ const setupInput = function() {
 
 // on any input from stdin (standard input), check to see if it is CTRL+C and then exit if it is
 const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
+  
+  switch (key) {
+    case 'w':
+      console.log('Move: up');
+      setInterval(()=>{
+        connection.write('Move: up');
+      },50);
+      break;
+    case 'a':
+      console.log('Move: left');
+      setInterval(()=>{
+        connection.write('Move: left');
+      },50);
+      break;
+    case 's':
+      console.log('Move: down');
+      setInterval(()=>{
+        connection.write('Move: down');
+      },50);
+      break;
+    case 'd':
+      console.log('Move: right');
+      setInterval(()=>{
+        connection.write('Move: right');
+      },50);
+      break;
+    case '1':
+      console.log("Say: yeet!");
+      connection.write("Say: yeet!");
+      break;
+    case '2':
+      console.log("Say: g'day!");
+      connection.write("Say: g'day!");
+      break;  
+    case '\u0003':
+      process.exit();
+    }
 }
 
 module.exports = {setupInput};
--- play.js
+++ play.js
@@ -4,6 +4,5 @@ const {connect} = require('./client');
 const {setupInput} = require('./input');
 
 console.log('Connecting ...');
-connect();
 
-setupInput();
+setupInput(connect());
