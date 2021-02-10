cdd78ce14efa7fca444fe4d84e15a2263b06c067
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-10 17:23:58 +0000

    trialed movement in client.js

--- client.js
+++ client.js
@@ -14,10 +14,16 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
-  conn.on('connect', ()=>{
+  conn.on('connect', ()=>{ //connect and register our name
     console.log('Successfully co-snek-ted to game server!');
-    conn.write('Name: SOL');
+    conn.write('Name: MYS');
   });
+
+  conn.on('connect', ()=> { //begin sending movement data
+    setInterval(()=>{
+      conn.write('Move: up'); //could also be 'Move: down', 'Move: left', 'Move: right' 
+    },200);
+  })
 
   conn.on('data', (data) => {
     console.log('Server says: ', data);
